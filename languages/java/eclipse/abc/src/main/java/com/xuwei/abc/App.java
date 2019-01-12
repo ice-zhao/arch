09b1afcfb6509916c97b36fd429e1337ec361df8
ice.zhao <smilebit@163.com>
2019-01-12 11:10:09 +0800

    update .gitignore file.

--- languages/java/eclipse/abc/src/main/java/com/xuwei/abc/App.java
+++ languages/java/eclipse/abc/src/main/java/com/xuwei/abc/App.java
@@ -1,15 +1,15 @@
 package com.xuwei.abc;
-//import org.hibernate.cfg.Configuration;
-//import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
 
 
 public class App 
 {
     public static void main( String[] args )
     {
-//    	Configuration hbmcfg = new Configuration();
-//    	SessionFactory sf = hbmcfg.configure("com/xuwei/abc/hibernate.cfg.xml")
-//    			.buildSessionFactory();
+    	Configuration hbmcfg = new Configuration();
+    	SessionFactory sf = hbmcfg.configure("com/xuwei/abc/hibernate.cfg.xml")
+    			.buildSessionFactory();
     
         System.out.println( "hi" );
     }
