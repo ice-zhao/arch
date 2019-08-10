62c7a822f94d30652adcaa6e5de7ef687aac89ad
ice.zhao <smilebit@163.com>
2019-08-10 14:35:49 +0800

    add aiabc project.

---
+++ languages/python/aiabc/src/main.py
@@ -0,0 +1,15 @@
+#
+#  @Author: wolfgang.zhao
+#  @Date: 2019-08-10  14:31
+#  @Mail: smallbit@163.com
+#  @GitHub: https://github.com/ice-zhao
+#
+from numpy import *
+
+
+def main():
+    print("hello ai")
+
+
+if __name__ == '__main__':
+    main()
